cd9edd710cb01c3e4d14e371d0498e65d297d21a
olesi4ka777 <olesi4ka87@gmail.com>
2023-10-26 14:26:32 +0300

    in work

---
+++ src/main/java/Triangle.java
@@ -0,0 +1,19 @@
+import javafx.scene.shape.TriangleMesh;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+               double s, p, a, b, c;
+
+       double a = a;
+       double b = b;
+       double c = c ;
+
+    public Triangle (double a, double b, double c)throw Unnat
+        p = (a + b + c) / 2;
+        s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+
+        System.out.printf("Area = %.2f%n", s);
+    }
+}
--- src/main/java/ipap/oppo/HelloController.java
+++ src/main/java/ipap/oppo/HelloController.java
@@ -1,57 +1,64 @@
 package ipap.oppo;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-    public class HelloController {
+public class HelloController {
 
+    @FXML
+    private Label Labelres;
+
+    @FXML
+    private TextField txtA;
+
+    @FXML
+    private TextField txtB;
+
+    @FXML
+    private TextField txtC;
+
+    @FXML
+    void area() {
+        double a = Double.parseDouble(txtA.getText());
+        double b = Double.parseDouble(txtB.getText());
+        double c = Double.parseDouble(txtC.getText());
+        double s = a + b + c * 2;
+        Labelres.setText("Result this" + s);
+    }
         @FXML
-        private Label Labelres;
-
-        @FXML
-        private Label txtA;
-
-        @FXML
-        private Label txtB;
-
-        @FXML
-        void divide(ActionEvent event) {
-            double a=Double.parseDouble(txtA.getText());
-            double b=Double.parseDouble(txtB.getText());
-            double c=a/b;
-            Labelres.setText("Result this"+c);
+        void divide () {
+            double a = Double.parseDouble(txtA.getText());
+            double b = Double.parseDouble(txtB.getText());
+            double c = a / b;
+            Labelres.setText("Result this" + c);
         }
 
         @FXML
-        void minus(ActionEvent event) {
-            double a=Double.parseDouble(txtA.getText());
-            double b=Double.parseDouble(txtB.getText());
-            double c=a-b;
-            Labelres.setText("Result this"+c);
+        void minus () {
+            double a = Double.parseDouble(txtA.getText());
+            double b = Double.parseDouble(txtB.getText());
+            double c = a - b;
+            Labelres.setText("Result this" + c);
         }
 
         @FXML
-        void multiplay(ActionEvent event) {
-            double a=Double.parseDouble(txtA.getText());
-            double b=Double.parseDouble(txtB.getText());
-            double c=a*b;
-            Labelres.setText("Result this"+c);
+        void multiplay () {
+            double a = Double.parseDouble(txtA.getText());
+            double b = Double.parseDouble(txtB.getText());
+            double c = a * b;
+            Labelres.setText("Result this" + c);
         }
 
         @FXML
-        void summa(ActionEvent event) {
-            double a=Double.parseDouble(txtA.getText());
-            double b=Double.parseDouble(txtB.getText());
-            double c=a+b;
-            Labelres.setText("Result this"+c);
+        void summa () {
+            double a = Double.parseDouble(txtA.getText());
+            double b = Double.parseDouble(txtB.getText());
+            double c = a + b;
+            Labelres.setText("Result this" + c);
         }
 
     }
-
-
-
 
 
 
